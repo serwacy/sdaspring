1d05b97475c549c76ddb46120334d6c8fe78a942
Michal Bojanowski <bojanowski.michal89@gmail.com>
2020-01-03 18:12:29 +0100

    Starting code

---
+++ src/main/java/pl/sdacademy/wiosnademo/WiosnaDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.wiosnademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiosnaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiosnaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sdacademy/wiosnademo/WiosnaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.wiosnademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiosnaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
